a144995f37d0b057199c86957a5c271d6f36e9e2
Yudita20 <yudikhanodia2005@gmail.com>
2026-03-06 11:11:28 +0530

    Check if sorted or not

---
+++ Array/Array_Easy/3_CheckSortedArray.py
@@ -0,0 +1,14 @@
+def sorted_array(arr,low,high):
+    # TC:O(N)
+    # SC:O(1)
+
+    for i in range(low,high-1):
+        if arr[i] > arr[i+1]:
+            return False
+
+    return True
+
+nums = [3,3,6]
+print(sorted_array(nums,0,len(nums)))
+
+
