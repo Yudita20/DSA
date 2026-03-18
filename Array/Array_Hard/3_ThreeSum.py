9477c04e0b9c3d5b2ff592e88ea068d377e5afde
Yudita20 <yudikhanodia2005@gmail.com>
2026-03-18 22:36:52 +0530

    Three Sum

---
+++ Array/Array_Hard/3_ThreeSum.py
@@ -0,0 +1,35 @@
+def three_sum(arr):
+    arr.sort()
+    res = []
+
+    for i in range(len(arr)):
+        if i>0 and arr[i] == arr[i-1]:
+            continue
+
+        left = i + 1
+        right = len(arr)-1
+
+        while left < right:
+            total = arr[i] + arr[left] + arr[right]
+
+            if total == 0:
+                res.append([arr[i],arr[left],arr[right]])
+                left += 1
+                right -= 1
+
+                while left<right and arr[i] == arr[i-1]:
+                    left += 1
+
+                while left<right and arr[right] == arr[right+1]:
+                    right -= 1
+
+            elif total<0:
+                left += 1
+            else:
+                right -= 1
+
+    return res
+
+
+nums = [-1,0,1,2,-1,-4,0,2]
+print(three_sum(nums))
