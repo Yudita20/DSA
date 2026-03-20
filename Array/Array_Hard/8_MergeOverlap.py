abcd7ccdaccd699db4976aedeb31c455938cba6c
Yudita20 <yudikhanodia2005@gmail.com>
2026-03-20 23:32:52 +0530

    Merge Overlapping subintervals

---
+++ Array/Array_Hard/8_MergeOverlap.py
@@ -0,0 +1,38 @@
+def merge_overlap_brute(arr):
+    arr.sort()
+    res = []
+    n = len(arr)
+    i = 0
+
+    while i < n:
+        start = arr[i][0]
+        end = arr[i][1]
+
+        j = i + 1
+
+        while j < n and arr[j][0] <= end:
+            end = max(end,arr[j][1])
+            j += 1
+
+        i = j  #Important condition
+
+        res.append([start,end])
+
+    return res
+
+def merge_overlap(arr):
+    arr.sort()
+    res = []
+
+    for interval in arr:
+        if not res or res[-1][1] < interval[0]:
+            res.append(interval)
+
+        else:
+            res[-1][1] = max(res[-1][1] , interval[1])
+
+    return res
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge_overlap_brute(intervals))
+print(merge_overlap(intervals))
