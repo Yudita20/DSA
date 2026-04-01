52c699c594ec44ad95535cc9ad6e284819dd893a
Yudita20 <yudikhanodia2005@gmail.com>
2026-04-01 21:21:14 +0530

    Find minimum in sorted and rotated

--- BinarySearch/11_FindMinimumInRotatedAndSorted.py
+++ BinarySearch/11_FindMinimumInRotatedAndSorted.py
@@ -7,10 +7,17 @@ def minimum_in_rotated(arr):
         if arr[mid] > arr[right]:
             left = mid + 1
 
-        else:
+        elif arr[mid] < arr[right]:
             right = mid
+
+        else:
+            right -= 1
 
     return arr[left]
 
 nums = [4,5,6,7,0,1,2,3]
 print(minimum_in_rotated(nums))
+
+# T(n) : O(log n) (best case)  and O(n) (worst case)
+                                   #[2,2,2,2,2]
+
