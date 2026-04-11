33133b7ddbf20721d7c254ad32f42d2ebdf84653
Yudita20 <yudikhanodia2005@gmail.com>
2026-04-11 19:23:59 +0530

    Search in sorted and rotated array

--- BinarySearch/8_SearchInSortedAndRotated.py
+++ BinarySearch/8_SearchInSortedAndRotated.py
@@ -30,8 +30,9 @@ def rotated_and_sorted(arr , tar):
 
 def search(arr,k):
     s , e = 0 , len(arr)-1
+
     while s <= e:
-        mid = (s+e) // 2
+        mid = (s+e)//2
 
         if arr[mid] == k:
             return mid
@@ -41,15 +42,19 @@ def search(arr,k):
                 e = mid - 1
             else:
                 s = mid + 1
+
         else:
             if arr[mid] < k <= arr[e]:
                 s = mid + 1
             else:
-                e = mid - 1
+                e = mid -1
+
     return -1
 
-nums = [0,1,2,3,4,5,6]
+
+nums = [4,5,6,7,8,0,1,2,3]
 print(rotated_and_sorted(nums,1))
-print(search(nums,7))
+print(search(nums,5))
+
 
 
