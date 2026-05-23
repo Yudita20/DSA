d390c7a9ed420ef0d13124542f121e3cfe537b2e
Yudita20 <yudikhanodia2005@gmail.com>
2026-05-23 20:45:23 +0530

    Reverse the words in the string

--- String/2_ReverseTheString.py
+++ String/2_ReverseTheString.py
@@ -9,26 +9,27 @@ def reverse_string(s):
 
     return result
 
-# def reverse(s):
-#     result = ""
-#     i = j = len(s)-1
-#     while j >= 0:
-#         while s[j] == " " and j >= 0:
-#             j -= 1
-#
-#         if j < 0:
-#             break
-#
-#         i = j
-#         while j >= 0 and s[j] != " ":
-#             j -= 1
-#
-#         result += s[j + 1: i + 1]
-#         if j > 0:
-#             result += " "
-#
-#     return result
+def reverse(s):
+    result = ""
+    i = j = len(s)-1
+    while j >= 0:
+        while j >= 0 and s[j] == " ":
+            j -= 1
+
+        if j < 0:
+            break
+
+        i = j
+        while j >= 0 and s[j] != " ":
+            j -= 1
+
+        result += s[j + 1: i + 1]
+
+        if j > 0:
+            result += " "
+
+    return result.strip()
 
 org_str = "  hello world  "
-# print(reverse(org_str))
+print(reverse(org_str))
 print(reverse_string(org_str))
