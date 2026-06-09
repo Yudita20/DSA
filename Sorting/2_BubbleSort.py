47ebd952574e843e3ddd4046f798a46d6f19a50e
Yudita20 <yudikhanodia2005@gmail.com>
2026-06-09 22:13:56 +0530

    Optimize the bubble sort

--- Sorting/2_BubbleSort.py
+++ Sorting/2_BubbleSort.py
@@ -1,22 +1,17 @@
 def bubble_sort(arr):
-    initial_sorted = True
     for i in range(len(arr)-1,-1,-1):
         swap = 0
         for j in range(0,i):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
                 swap += 1
-        #Check swap for 1 pass
-        if i == len(arr)-1 and swap>0:
-            initial_sorted = False
 
-    #Already sorted
-    if initial_sorted:
-        print("Already sorted")
-        return arr
+        #Check swap for 1 pass
+        if i == len(arr)-1 and swap == 0:
+            return arr
 
     return arr
 
 
-nums = [10,3,45,62]
+nums = [1,2,3,4,5]
 print(bubble_sort(nums))
