fc7dd7e2849229938f4ac64901b51c903a3ea181
Yudita <yudikhanodia2005@gmail.com>
2026-06-19 11:01:29 +0530

    Detect Cycle in a Linked list

---
+++ Linked_List/SinglyLL/6_DetectCycleLL.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data1, next1=None):
+        self.data = data1
+        self.next = next1
+
+class Solution:
+    def detectLoop(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
+
+    def detectLoopBrute(self , head):
+        hash_map = {}
+
+        t = head
+        while t:
+            if t in hash_map:
+                return True
+
+            hash_map[t] = t.next
+            t = t.next
+        return False
+
+# Driver code
+if __name__ == "__main__":
+    head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+    fifth = Node(5)
+
+    head.next = second
+    second.next = third
+    third.next = fourth
+    fourth.next = fifth
+    # Create a loop
+    fifth.next = third
+
+    sol = Solution()
+
+    if sol.detectLoopBrute(head):
+        print("Loop detected in the linked list.")
+    else:
+        print("No loop detected in the linked list.")
