3388ab7cf63837cf9f14f3ea1a8d19a805a2a49e
Yudita <yudikhanodia2005@gmail.com>
2026-07-09 13:08:08 +0530

    Basic of Doubly Linked List

--- Linked_List/DoublyLL/1_Basics.py
+++ Linked_List/DoublyLL/1_Basics.py
@@ -1,30 +1,31 @@
 class Node:
-    def __init__(self , data , prev=None , next=None):
+    def __init__(self, data, prev=None, next=None):
         self.prev = prev
         self.data = data
         self.next = next
 
 
-class DoublyLL:
-    def __init__(self,head=None):
+class doublyLL:
+    def __init__(self, head=None):
         self.head = head
 
-    def insert_at_end(self,value):
+    def insert_at_end(self, value):
         temp = Node(value)
 
         if self.head is None:
             self.head = temp
             return
 
-        t1 = self.head
-        while t1.next is not None:
-            t1 = t1.next
+        t = self.head
+        while t.next is not None:
+            t = t.next
 
-        t1.next = temp
-        temp.prev = t1
+        t.next = temp
+        temp.prev = t
 
-    def insert_at_beg(self,value):
-        temp =Node(value)
+    def insert_at_beg(self, value):
+        temp = Node(value)
+
         if self.head is None:
             self.head = temp
             return
@@ -33,61 +34,88 @@ class DoublyLL:
         self.head.prev = temp
         self.head = temp
 
-    # Inserting after the key
-    def insert_at_mid(self,value,key):
+    def insert_at_mid(self, value, after_value):
         temp = Node(value)
-        t1 = self.head
 
-        while t1 is not None:
-            if t1.data == key:
-                break
-            else:
-                t1 = t1.next
-        temp.next = t1.next
-        t1.next.prev = temp
-        temp.prev = t1
-        t1.next = temp
-
-    def deletionDll(self,value):
         if self.head is None:
-            print("List is empty")
-            return
-
-        if self.head.data == value:
-            #Head is the node to be deleted
-            self.head = self.head.next
-            self.head.prev = None
+            self.head = temp
             return
 
         t = self.head
         while t is not None:
-            # Last Node
-            if t.next is None:
-                if t.data == value:
-                    t.prev.next = None
-                return
-
-            # Middle Node
-            if t.data == value:
-                t.prev.next = t.next
-                t.next.prev = t.prev
+            if t.data == after_value:
+                temp.next = t.next
+                if t.next is not None:
+                    t.next.prev = temp
+                temp.prev = t
+                t.next = temp
                 return
             t = t.next
 
+    def deletion_from_end(self):
+        if self.head is None:
+            return
 
+        if self.head.next is None:
+            self.head = None
+            return
+
+        t = self.head
+        while t.next is not None:
+            t = t.next
+
+        t.prev.next = None
+
+    def deletion_from_beg(self):
+        if self.head is None:
+            return
+
+        self.head = self.head.next
+        if self.head is not None:
+            self.head.prev = None
+
+    def deletion_from_mid(self, value):
+        if self.head is None:
+            return
+
+        t = self.head
+        while t is not None:
+            if t.data == value:
+                if t == self.head:
+                    self.deletion_from_beg()
+                elif t.next is None:
+                    self.deletion_from_end()
+                else:
+                    t.prev.next = t.next
+                    t.next.prev = t.prev
+                return
+            t = t.next
 
     def printLL(self):
-        t1 = self.head
-        while t1 is not None:
-            print(t1.data)
-            t1 = t1.next
+        t = self.head
+        while t is not None:
+            print(t.data)
+            t = t.next
 
-obj = DoublyLL()
+
+obj = doublyLL()
 obj.insert_at_end(10)
 obj.insert_at_end(20)
 obj.insert_at_end(30)
+obj.insert_at_end(40)
+obj.insert_at_end(50)
 obj.insert_at_beg(5)
-obj.insert_at_mid(15,10)
+obj.insert_at_mid(15, 10)
+obj.deletion_from_end()
+obj.deletion_from_beg()
+obj.deletion_from_mid(15)
 obj.printLL()
+
+
+
+
+
+
+
 
 
