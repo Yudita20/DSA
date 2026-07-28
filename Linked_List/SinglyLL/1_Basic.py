ed7a5b7462da287315d95838d180ece1615f2b4f
Yudita <yudikhanodia2005@gmail.com>
2026-07-28 22:42:40 +0530

    Singly Linked list CRUD operations

--- Linked_List/SinglyLL/1_Basic.py
+++ Linked_List/SinglyLL/1_Basic.py
@@ -1,29 +1,28 @@
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self,data,next=None):
         self.data = data
         self.next = next
 
 class SinglyLL:
-    def __init__(self, head=None):
+    def __init__(self , head=None):
         self.head = head
 
-    def insert_at_end(self , value):
+    def insertAtEnd(self,value):
         temp = Node(value)
+
         if self.head is None:
             self.head = temp
             return
 
-        t1 = self.head
-        while t1.next is not None:
-            t1 = t1.next
+        t = self.head
+        while t.next is not None:
+            t = t.next
 
-        t1.next = temp
+        t.next = temp
 
-
-    def insert_at_beg(self , value):
+    def insertAtBeg(self, value):
         temp = Node(value)
 
-        # inserting when list is empty
         if self.head is None:
             self.head = temp
             return
@@ -31,41 +30,18 @@ class SinglyLL:
         temp.next = self.head
         self.head = temp
 
-    def insert_at_mid(self,value,after_data_value):
+    def insertAtMid(self, value, after_data):
         temp = Node(value)
-        t = self.head
 
-        while t is not None:
-            if t.data == after_data_value:
+        t = self.head
+        while t.next is not None:
+            if t.data == after_data:
                 temp.next = t.next
                 t.next = temp
                 return
-
             t = t.next
 
-    def delete_from_beg(self):
-        if self.head is None:
-            return
-
-        self.head = self.head.next
-
-    def delete_from_end(self):
-        if self.head is None:
-            return
-
-        if self.head.next is None:
-            self.head = None
-            return
-
-        t1 = self.head
-        prev = t1
-        while t1.next is not None:
-            prev = t1
-            t1 = t1.next
-
-        prev.next = None
-
-    def delete_from_mid(self , value):
+    def deletionLL(self,value):
         if self.head is None:
             return
 
@@ -73,33 +49,33 @@ class SinglyLL:
             self.head = self.head.next
             return
 
-        prev = self.head
-        temp = self.head.next
-
-        while temp is not None:
-            if temp.data == value:
-                prev.next = temp.next
+        p = self.head
+        t = self.head.next
+        while t.next is not None:
+            if t.data == value:
+                p.next = t.next
                 return
-            prev = temp
-            temp = temp.next
+            p = t
+            t = t.next
+        p.next = None
 
     def printLL(self):
-        t1 = self.head
-        while t1 is not None:
-            print(t1.data , end =" ")
-            t1 = t1.next
+        t = self.head
+        while t is not None:
+            print(t.data)
+            t = t.next
+
 
 obj = SinglyLL()
-obj.insert_at_end(10)
-obj.insert_at_end(20)
-obj.insert_at_end(30)
-obj.insert_at_beg(5)
-obj.insert_at_mid(15,10)
-obj.delete_from_beg()
-obj.delete_from_end()
-obj.delete_from_mid(15)
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtEnd(40)
+obj.insertAtBeg(5)
+obj.insertAtBeg(2)
+obj.insertAtMid(7,5)
+obj.deletionLL(7)
 obj.printLL()
-
 
 
 
