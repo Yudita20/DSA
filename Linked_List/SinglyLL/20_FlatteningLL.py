4db425ceab3ad3606811f9ed1103a9ba2d517908
Yudita <yudikhanodia2005@gmail.com>
2026-09-02 21:11:13 +0530

    Flattening of linked list

---
+++ Linked_List/SinglyLL/20_FlatteningLL.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, data, next=None, child = None):
+        self.data = data
+        self.next = next
+        self.child = child
+
+
+class Solution:
+    def mergeLL(self, head1, head2):
+        dummy = Node(-1)
+        res = dummy
+
+        while head1 is not None and head2 is not None:
+            if head1.data < head2.data:
+                res.child = head1
+                res = head1
+                head1 = head1.child
+            else:
+                res.child = head2
+                res = head2
+                head2 = head2.child
+            res.next = None
+
+        if head1:
+            res.child = head1
+        else:
+            res.child = head2
+
+        if dummy.child:
+            dummy.child.next = None
+
+        return dummy.child
+
+    def flattenLL(self, head):
+        if head is None or head.next is None:
+            return head
+
+        new_head = self.flattenLL(head.next)
+
+        merged_head = self.mergeLL(head, new_head)
+        return merged_head
+
+
+    def printLL(self, head):
+        while head is not None:
+            print(head.data, end=" ")
+            head = head.child
+
+
+if __name__ == "__main__":
+    head = Node(5)
+    head.child = Node(14)
+
+    head.next = Node(10)
+    head.next.child = Node(14)
+
+    head.next.next = Node(12)
+    head.next.next.child = Node(20)
+    head.next.next.child.child = Node(23)
+
+    head.next.next.next = Node(7)
+    head.next.next.next.child = Node(17)
+
+    sol = Solution()
+    new_head = sol.flattenLL(head)
+    sol.printLL(new_head)
+
+
+
+
+
