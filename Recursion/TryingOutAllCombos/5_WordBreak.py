9b41cc8dc0433f54edfcccc2eb854cd3c1962a1a
Yudita <yudikhanodia2005@gmail.com>
2026-09-20 11:10:27 +0530

    Word break

---
+++ Recursion/TryingOutAllCombos/5_WordBreak.py
@@ -0,0 +1,24 @@
+def isExist(s, word_dct):
+    return s in word_dct
+
+def wordBreak(s, word_dct, idx = 0, curr_st = ""):
+    if idx == len(s):
+        return True
+
+
+    for i in range(idx, len(s)):
+        curr_st += s[i]
+        if isExist(curr_st, word_dct):
+
+            if wordBreak(s, word_dct, i + 1, ""):
+                return True
+
+            curr_st = curr_st[: len(curr_st) -1]
+
+    return False
+
+
+if __name__ == "__main__":
+    st = "takeuforward"
+    word_dict = ["take" , "u" , "you", "forward"]
+    print(wordBreak(st, word_dict))
